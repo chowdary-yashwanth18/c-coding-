f1aa8b86c12b0a2abf206b5ff342c3bac0429ac4
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:03:20 +0530

    Add files via upload

---
+++ if else on voting eligibility.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf(" enter age:");
+	scanf("%d",&age);
+	if(age>=18)
+	{
+		printf("the person is eligible to vote");
+		
+	}
+	else
+	{
+		printf("the person is not eligible to vote");
+		
+	}
+	return 0;
+}

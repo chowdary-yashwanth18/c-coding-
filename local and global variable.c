f32a31a9bb9e4a27d7d7bfa859657f8700644024
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 06:57:25 +0530

    Add files via upload

---
+++ local and global variable.c
@@ -0,0 +1,19 @@
+//program on local and global variables.
+#include<stdio.h>
+int roll_num=195;
+int main()
+{
+	
+	printf("roll number is %d",roll_num);
+	return 0;
+}
+
+/*
+#include<stdio.h>
+int main()
+{
+	int roll_number=195;
+	printf("roll number is %d",roll_number);
+	return 0;
+}
+*/

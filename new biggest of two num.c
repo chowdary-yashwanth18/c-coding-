2f2369cc5bf0dc0f372c37d679e64f3a29f45689
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:14:35 +0530

    Add files via upload

---
+++ new biggest of two num.c
@@ -0,0 +1,21 @@
+//program to find biggest of two numbers by using simple if stmt.
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a,b values:");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("a is bigger than b \n ");
+    }
+    /* if(b>a)
+    {
+    	printf("b is bigger than a");
+	} */
+	{
+		printf("a is  not bigger number \n");
+	}
+	return 0;
+}
+

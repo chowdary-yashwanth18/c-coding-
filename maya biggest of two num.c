e15c32fb482186e42168c615ffb74e7d947119b6
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 10:49:23 +0530

    Update maya biggest of two num.c

--- maya biggest of two num.c
+++ maya biggest of two num.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int T;
-    scanf("%d", &T);
+int main() 
+{
+    int x, y;
 
-    while (T--) {
-        int X, N;
-        scanf("%d %d", &X, &N);
+   
+    scanf("%d", &x);
+    scanf("%d", &y);
 
-        // Each test case is worth X/10 points
-        int points_per_case = X / 10;
-
-        // Chef's score = points per case * number of cases passed
-        int score = points_per_case * N;
-
-        printf("%d\n", score);
+  
+    if (x > y)
+    {
+        printf("%d
+", x);
+    } else 
+    {
+        printf("%d
+", y);
     }
 
     return 0;

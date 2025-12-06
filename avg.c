394a1c4655f2bbc0df25f545ea57b56e81e20933
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:09:54 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,11 @@
+//program for average of three numbers.
+#include<stdio.h>'
+int main()
+{
+
+int a,b,c,avg;
+printf("enter three values:");
+scanf("%d%d%d",&a,&b,&c);
+avg=(a+b+c)/3;
+printf("average of three numbers is %d",avg);
+}

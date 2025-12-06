c7e55cef2b346c6c8d9a5f032ccc9f72d0479092
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:11:03 +0530

    Add files via upload

---
+++ sizeof operator.c
@@ -0,0 +1,24 @@
+//program on sizeof operator.
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	double c;
+	char d;
+	long e;
+	printf("size of int=%lu bytes\n",sizeof(a));
+	
+	printf("size of float=%lu bytes\n",sizeof(b));
+
+	printf("size of double=%lu bytes\n",sizeof(c));
+
+	printf("size of char=%lu bytes\n",sizeof(d));
+	
+	printf("size of long=%lu bytes\n",sizeof(e));
+	
+	return 0;
+	
+
+	
+}

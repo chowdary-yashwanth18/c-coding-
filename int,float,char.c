c4fd57990a9800e7c427531d7de50c79b1b51850
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:05:50 +0530

    Add files via upload

---
+++ int,float,char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    int a = 0;
+    char b = 'r';
+    float c = 2.8;
+    printf("The address of a is %d: the value of a is %d ",&a,a );
+    printf("The address of b is %c: the value of a is %d ",&b,b );
+    printf("The address of c is %d: the value of a is %d ",&c,c);
+}

0f1738d18ea1826ff8cdebf2cd7e3a3822744a5c
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:13:31 +0530

    Add files via upload

---
+++ max of three num.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the values for a,b,c:");
+	scanf("%d%d%d",&a,&b,&c);
+	
+	if ( a > b ) {
+		if ( a > c ) {
+			printf("a is maximum");
+		}
+		
+		else {
+			printf("c is maximum");
+		}
+	}
+	
+	if ( a < b ) {
+		if ( b > c ) {
+			printf("b is maximum");
+		}
+		
+		else {
+			printf("c is maximum");
+		}
+	}
+	
+	
+	return 0;
+}

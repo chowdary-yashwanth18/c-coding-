bc2d4da921efed74e616f0078ed1c1cb87e3e26f
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 09:25:09 +0530

    Add files via upload

---
+++ lc two sum.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int* result = (int*)malloc(sizeof(int) * 2);
+
+    // Brute force approach (simple and accepted)
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+
+    return result; // Should never reach here if input guarantees a solution
+}

68c1fa1e5155601521b134cdf92c2e3eaad0fda8
CHOWDARY YASHWANTH <chowdaryyashwanth18@gmail.com>
2025-12-06 07:01:39 +0530

    Add files via upload

---
+++ factorial recurrsive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int factorial (int i)
+{
+	if(i==1)
+	{
+	return 1;
+    }   
+
+return i*factorial(i-1);
+}
+int main()
+{
+	printf("factorial of a number is %d",factorial(5));
+	return 0;
+}
